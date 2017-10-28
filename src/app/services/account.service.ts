047f2208fff332350f9072e1b7af85418065d727
Sekhar <sekhar.vanka@gmail.com>
2017-10-28 15:41:56 +1100

    removing reduntant code

--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -23,36 +23,10 @@ export class AccountService {
         }
       }
     };
-    //console.log(body);
-    //let test = new RequestOptionsArgs()
-    let headers = new Headers();
-    //headers.append('Content-Type', 'application/json');
-    headers.append('x-nab-key', '0c12ce1a-f5a5-4933-b5aa-c27e14c757d7');
 
 
-    var result = this.http.post('https://hackathon.api.extnp.nab.com.au/v2/auth?v=1',body,{Headers: headers});
+
+    var result = this.http.post('https://hackathon.api.extnp.nab.com.au/v2/auth?v=1',body);
     result.subscribe();//TODO : get Authorization token.
    }
-
-
-
-
-
-
-   /*{
-    "loginRequest": {
-      "brand": "nab",
-      "lob": "nab",
-      "credentials": {
-        "apiStructType": "usernamePassword",
-        "usernamePassword": {
-          "username": "10055670",
-          "password": "aaa111"
-        }
-      }
-    }
-  }*/
-
-
-
 }
