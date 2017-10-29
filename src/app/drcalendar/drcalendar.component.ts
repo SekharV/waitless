6accee1c6f878ef84cf69c77460ed0f5b999a9f5
rajesh krishnan <getmetorajesh@gmail.com>
2017-10-29 14:19:55 +1100

    dynamically updating requests and appointments

--- src/app/drcalendar/drcalendar.component.ts
+++ src/app/drcalendar/drcalendar.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./drcalendar.component.scss']
 })
 export class DrCalendarComponent implements OnInit {
-
+  public leads = [];
+  public appointments = [];
   constructor() { }
 
   ngOnInit() {
+    this.appointments = [
+      { date: '29/Oct/2017', detail: 'Surgery(Mr. Joe - Knee replacemnt)'},
+      { date: '30/Oct/2017', detail: 'Post surgery checkup(Miss. Elton)'},
+      { date: '10/Nov/2017', detail: 'Appointment(Mr. Davin Jones)'},
+      { date: '16/Nov/2017', detail: 'Surgery(Mr. John doe - Knee replacemnt)' },
+    ];
+    this.leads = [
+      { id: 1, date: '20/Nov/2017', detail: 'Appointment request for(Mr. Milton - Knee replacement)'},
+      { id: 2, date: '21/Nov/2017', detail: 'Appointment request for(Mr. Milton - Knee replacement)'},
+      { id: 3, date: '10/Dec/2017', detail: 'Appointment request for(Mr. Milton - Knee replacement)' },
+      { id: 4, date: '04/Dec/2017', detail: 'Appointment request for(Mr. Milton - Knee replacement)'}
+    ];
   }
+  handleChange(event , id) {
+    const lead = this.leads.filter(item => item.id === id)[0];
+    this.appointments.push( {date: lead.date, detail: lead.detail });
+    this.leads = this.leads.filter(item => item.id !== lead.id);
+  }
+
 
 }
